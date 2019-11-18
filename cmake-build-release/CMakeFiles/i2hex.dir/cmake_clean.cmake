2fb0fc5977ee544b5b6308050f4ac43ec302e3f4
MagnavoxTG <dominik.lohner92@gmx.at>
2019-11-18 12:23:43 +0100

    Release 1.0

---
+++ cmake-build-release/CMakeFiles/i2hex.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/i2hex.dir/main.c.o"
+  "i2hex"
+  "i2hex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/i2hex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-release/CMakeFiles/i2hex.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for i2hex.
+# This may be replaced when dependencies are built.
